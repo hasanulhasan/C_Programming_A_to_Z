77c993b82693d8f01017172af96b8048f21ba26b
hasanulhasan <hasanlink71@yahoo.com>
2025-02-12 12:25:41 +0600

    jadu matric problem solved

---
+++ Jadu_Matrix/Jadu_Matrix.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int diagonalUnitArrCheck(int row, int col, int arr[row][col]) {
+    if (row != col) {
+        return 0;
+    }
+    int n = row;
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            if (i == j || i + j == n - 1) {  
+                if (arr[i][j] != 1) {
+                    return 0;  
+                }
+            } else {  
+                if (arr[i][j] != 0) {
+                    return 0;
+                }
+            }
+        }
+    }
+    return 1;
+}
+
+int main() {
+    int row, col;
+    scanf("%d %d", &row, &col);
+    int arr[row][col];
+    
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+    
+    if (diagonalUnitArrCheck(row, col, arr)) {
+        printf("YES");
+    } else {
+        printf("NO");
+    }
+
+    return 0;
+}
